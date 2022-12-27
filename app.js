d8f645b6184fefb3f0bf20162bad7d4d8c11aeb7
thevasanthram <thevasanthram@github.com>
2022-12-27 12:33:41 +0530

    directory for frontEnd added

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
 
 dotenv.config();
 
+// mongoDB connection
+try {
+  mongoose.connect(process.env.MONGOURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  const db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error: '));
+  db.once('open', function () {
+    console.log('Connected successfully');
+  });
+} catch (err) {
+  console.log('MongoDB: ', err);
+}
 const app = express();
 
 app.use(express.json());
