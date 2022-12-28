a142d5dc1bbb73b8b7542ddfbea01763cdf2b96e
thevasanthram <thevasanthram@github.com>
2022-12-28 10:57:37 +0530

    updated navigation bar

--- e-commerce/src/pages/NavBar.jsx
+++ e-commerce/src/pages/NavBar.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import './../styles/NavBar.css';
+import eCommerceLogo from './../images/eCommerce logo.png';
 function NavBar() {
   return (
-    <div>
-      <h1>Nav Bar</h1>
+    <div className='navBarDivision'>
+      <img src={eCommerceLogo} alt='logo' className='eCommerceLogo' />
+      <input type='text' name='searchBarValue' className='searchBar' />
+      <p className='navBarCart'>Cart</p>
+      <p className='navBarProfile'>Profile</p>
     </div>
   );
 }
